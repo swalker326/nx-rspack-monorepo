3440e4ce60cdbc4638c61ada8ddf7864c1851798
Shane <shane@swalker.dev>
2024-09-22 15:17:28 -0500

    added rspack host app

--- apps/host/src/app/app.tsx
+++ apps/host/src/app/app.tsx
@@ -1,12 +1,21 @@
+import * as React from 'react';
+
 import NxWelcome from './nx-welcome';
 
-import '../styles.css';
+import { Link, Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="host" />
-    </div>
+    <React.Suspense fallback={null}>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<NxWelcome title="host" />} />
+      </Routes>
+    </React.Suspense>
   );
 }
 
