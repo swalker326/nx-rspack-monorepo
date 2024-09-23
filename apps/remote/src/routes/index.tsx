ba655a2daa2891cddfe79ce58f17698e5ab39dde
Shane <shane@swalker.dev>
2024-09-22 19:16:58 -0500

    commit before adding zephyr

--- apps/remote/src/routes/index.tsx
+++ apps/remote/src/routes/index.tsx
@@ -1,8 +1,13 @@
 export default function FeedRoute() {
 	return (
 		<div>
-			<h1>Remote Feed</h1>
-			<p>App mounted from Feed Remote</p>
+			<h1 className="text-5xl">Remote App</h1>
+			<p>
+				App mounted from{" "}
+				<pre>
+					<code>/apps/remote/routes/index.tsx</code>
+				</pre>
+			</p>
 		</div>
 	);
 }
